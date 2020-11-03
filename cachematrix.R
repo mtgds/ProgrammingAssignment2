1029ead50c59895729c73da83897b3f27855424f
mtgds <73651118+mtgds@users.noreply.github.com>
2020-11-03 11:48:09 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Las siguientes dos funciones tienen como finalidad almacenar en caché
+## el inverso de una matriz. Se hará una descripción del paso a paso de
+## cada una de éstas
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Mediante esta función, se asigna un valor a una matriz en un environment
+## que es diferente al environment actual
+
+makeCacheMatrix <- function(cm = matrix()) {
+      
+      ## se inicializa la matriz inversa con valores nulos
+	cminversa <- NULL
+      
+	## se establece el valor de la matriz que se ingresa
+	## como argumento en la función makeCacheMatrix
+	set <- function (cm2){
+		cm <<- cm2
+		cminversa <<- NULL
+	}
+
+	## se establece el valor de la matriz inversa
+	get <- function()cm
+	setinversa <- function(inversa)cminversa <<- inversa
+
+	## se obtiene el valor de la matriz inversa
+	getinversa <- function()cminversa	
+	list (set=set,get=get, setinversa=setinversa, getinversa=getinversa)
 
 }
 
 
-## Write a short comment describing this function
+## Esta función calcula la inversa de la matriz creada, y verifica primero
+## si la inversa se ha calculado. Si es así, obtiene el vaklor de la matriz inversa 
+## del caché. En caso contrario, lo calcula.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(cm, ...) {
+ 
+	## verificación si la matriz inversa del argumento cm se ha calculado
+	cminversa <- cm$getinversa()
+	if(!is.null(cminversa)){
+		message("matriz inversa en caché")
+		return(cminversa)
+	}
+
+      ## cálculo de la matriz inversa
+	data <- cm$get()
+	cminversa <- solve(data,...)
+	cm$setinversa(cminversa)
+	cminversa
+
 }
